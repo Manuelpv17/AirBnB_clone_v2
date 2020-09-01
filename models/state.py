e154cd68c5efab81bebd30ee3aaf5037252d9452
Manuelpv17 <manuelpv17@outlook.com>
2020-09-01 06:47:06 -0500

    Add task 6 DB storage

--- models/state.py
+++ models/state.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import models
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
+    cities = relationship("City",
+                          backref='state', cascade="all, delete")
+
+    @property
+    def cities(self):
+        """ Getter for cities """
+        l = []
+        dic = models.storage.all(City)
+        for city in dic.values():
+            if city.state_id == self.id:
+                l.append(city)
+        return l
