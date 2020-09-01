496d2da5c5b1fd2912d81631067dc327b9e534ec
Manuelpv17 <manuelpv17@outlook.com>
2020-08-31 20:49:21 -0500

    Add task 5 update file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -56,6 +56,7 @@ class FileStorage:
             pass
 
     def delete(self, obj=None):
+        """ delete obj from __objects"""
         for key, value in FileStorage.__objects.items():
             if obj is value:
                 del FileStorage.__objects[key]
